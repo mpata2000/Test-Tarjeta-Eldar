a252e90bc366c1dbe83c402f37e87793d6770205
Martin Pata <mpata2000@gmail.com>
2022-08-23 11:49:10 -0300

    Tarjeta constructure

--- src/main/java/Tarjeta.java
+++ src/main/java/Tarjeta.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Tarjeta {
+public class Tarjeta {
+    private final int numero;
+    private final String titular;
+
+    public Tarjeta(String marca,int numero, String titular, String fechaVencimiento) {
+        this.numero = numero;
+        this.titular = titular;
+    }
 }
